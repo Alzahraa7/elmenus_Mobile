9bdbfc507d76ea7dada14ecd547a3931efe35b33
zahra <alzahraam209@gmail.com>
2022-03-15 04:01:35 +0200

    list, search, offer

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import * as React from 'react';
-import { Text, View} from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NativeBaseProvider} from "native-base";
-import Delivery from './Components/Delivery/Delivery';
-import DineOut from './Components/DineOut/DineOut';
-import Restaurant from './Components/Restaurant/Restaurant';
-import Me from './Components/Me/Me';
-import { db } from './Firebase/firebase';
-import { collection,getDocs } from 'firebase/firestore/lite';
+import * as React from "react";
+import { Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NativeBaseProvider } from "native-base";
+import Delivery from "./Components/Delivery/Delivery";
+import DineOut from "./Components/DineOut/DineOut";
+import Restaurant from "./Components/Restaurant/Restaurant";
+import Me from "./Components/Me/Me";
+import { db } from "./Firebase/firebase";
+import { collection, getDocs } from "firebase/firestore/lite";
 const Tab = createBottomTabNavigator();
 
 export default function App() {
@@ -22,38 +22,52 @@ export default function App() {
   // }getCities()
   return (
     <NativeBaseProvider>
-    <NavigationContainer>
-      <Tab.Navigator
-       screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
 
-          if (route.name === 'Delivery') {
-            iconName = focused
-              ? 'bicycle'
-              : 'bicycle-outline';
-          } else if (route.name === 'DineOut') {
-            iconName = focused ? 'fast-food' : 'fast-food-outline';
-          }
-          else if (route.name === 'Restaurant') {
-            iconName = focused ? 'restaurant' : 'restaurant-outline';
-          }
-          else if (route.name === 'Me') {
-            iconName = focused ? 'person' : 'person-outline';
-          }
+              if (route.name === "Delivery") {
+                iconName = focused ? "bicycle" : "bicycle-outline";
+              } else if (route.name === "DineOut") {
+                iconName = focused ? "fast-food" : "fast-food-outline";
+              } else if (route.name === "Restaurant") {
+                iconName = focused ? "restaurant" : "restaurant-outline";
+              } else if (route.name === "Me") {
+                iconName = focused ? "person" : "person-outline";
+              }
 
-          // You can return any component that you like here!
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: '#e32207',
-        tabBarInactiveTintColor: 'gray',
-      })}>
-        <Tab.Screen name="Delivery" component={Delivery} />
-        <Tab.Screen name="Restaurant" component={Restaurant} />
-        <Tab.Screen name="DineOut" component={DineOut} />
-        <Tab.Screen name="Me" component={Me} />
-      </Tab.Navigator>
-    </NavigationContainer>
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: "#e32207",
+            tabBarInactiveTintColor: "gray",
+          })}
+        >
+          <Tab.Screen
+            options={{
+              title:'Deliver To',
+              headerTitleContainerStyle:{
+                alignItems:'flex-start'
+              },
+              headerStyle: {
+                backgroundColor: "#FAF9F9",
+              },
+              headerTintColor: "#b1b1b1",
+              headerTitleStyle: {
+                fontWeight: '400',
+                fontSize:'17px'
+              },
+            }}
+            name="Delivery"
+            component={Delivery}
+          />
+          <Tab.Screen name="Restaurant" component={Restaurant} />
+          <Tab.Screen name="DineOut" component={DineOut} />
+          <Tab.Screen name="Me" component={Me} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
--- Components/Delivery/Delivery.js
+++ Components/Delivery/Delivery.js
@@ -1,10 +1,23 @@
-import { Text, View } from 'react-native';
+import { Box, Button, Slider, Image,Text, Input } from "native-base";
+import { Ionicons } from "@expo/vector-icons";
+import Search from "./Search";
+import IconsList from "./iconsList";
 const Delivery = () =>{
     return(
         <>
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Delivery!</Text>
-    </View>
+        <Box bgColor='white'>
+            <Search />
+            <Box margin='5' flexDirection='row'>
+            <Image width='50px' height='50px' source={{uri:'https://previews.123rf.com/images/arcady31/arcady311606/arcady31160600002/59113161-offre-sp%C3%A9ciale-ic%C3%B4ne-%C3%A9toile-rouge.jpg'}} > 
+            </Image>
+            <Text marginLeft='4' fontSize='14px' fontWeight='bold' >
+                Get 30 EGP discount on your 
+                FIRST THREE orders above 80 EGP with code 
+                THREE30 
+            </Text>
+            </Box>
+            <IconsList />
+        </Box>
         </>
     );
 }
