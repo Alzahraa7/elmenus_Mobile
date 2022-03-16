71f71ef00b8f3ff224b32b7ec3e9af3df3cc804c
Rami-Ayyad <engramiahmad97@gmail.com>
2022-03-16 14:38:21 +0200

    hi offers new branch

--- Components/Offers/Offers.js
+++ Components/Offers/Offers.js
@@ -4,7 +4,7 @@ const Offers = () => {
     return (
         <>
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Offers!</Text>
+                <Text>Offers</Text>
             </View>
         </>
     )
