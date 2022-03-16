b39dbfdfdee7d73e889055003dcaaa0afd628456
Rami-Ayyad <engramiahmad97@gmail.com>
2022-03-16 14:35:31 +0200

    hi offers

---
+++ Components/Offers/Offers.js
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+const Offers = () => {
+    return (
+        <>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Offers!</Text>
+            </View>
+        </>
+    )
+}
+
+export default Offers;
